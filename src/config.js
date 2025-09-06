ca34ffa73ac94a92097dc3684ea519e6929b3e97
abdellahsaidi <abdellahsaidi310309@gmail.com>
2025-09-06 23:50:07 +0100

    Update folder contents

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://portfolio-backend-0w1g.onrender.com";
 export default API_BASE_URL;
